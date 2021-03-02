b6829a1fa417a76e35fbe0686f4351fbd0967e23
victor-basis <victor@basis-ai.com>
2021-03-02 21:24:55 +0800

    Updated serve index page

--- serve.py
+++ serve.py
@@ -118,7 +118,7 @@ def get_inference():
 @app.route("/", methods=["GET"])
 def get_score():
     """Returns the help page"""
-    return "<h1 style='color:blue'>Hello Veritas Model Server!</h1>"
+    return "<h1 style='color:blue'>Hello Credit Scoring Model Server!</h1>"
 
 
 def main():
