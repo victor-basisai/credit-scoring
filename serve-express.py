e508fa4c4e09cf5ddf5411a52c9b7782fa7c7b1b
_ <victor@basis-ai.com>
2021-03-22 09:32:09 +0000

    updated to return json

--- serve-express.py
+++ serve-express.py
@@ -4,6 +4,34 @@ from typing import List, Optional
 from bedrock_client.bedrock.model import BaseModel
 
 
+# Ordered list of model features
+FEATURES = [ 
+    'LIMIT_BAL',
+    'SEX',
+    'EDUCATION',
+    'MARRIAGE',
+    'AGE',
+    'PAY_1',
+    'PAY_2',
+    'PAY_3',
+    'PAY_4',
+    'PAY_5',
+    'PAY_6',
+    'BILL_AMT1',
+    'BILL_AMT2',
+    'BILL_AMT3',
+    'BILL_AMT4',
+    'BILL_AMT5',
+    'BILL_AMT6',
+    'PAY_AMT1',
+    'PAY_AMT2',
+    'PAY_AMT3',
+    'PAY_AMT4',
+    'PAY_AMT5',
+    'PAY_AMT6'
+]
+
+
 class Model(BaseModel):
     def __init__(self, path: Optional[str] = None):
         '''
@@ -12,8 +40,17 @@ class Model(BaseModel):
         with open(path or "/artefact/model.pkl", "rb") as f:
             self.model = pickle.load(f)
 
-    def predict(self, features: List[List[float]]) -> List[float]:
+    def predict(self, request_json):
         '''
         Runs the prediction
         '''
-        return self.model.predict_proba(features)[:, 0].tolist()
+        # Parse request_json into ordered list
+        features = list()
+        for col in FEATURES:
+            features.append(request_json[col])
+        
+        # Return the result
+        result = {
+            "predicted_proba": self.model.predict_proba(np.array(features).reshape(1, -1))[:, 1].item()
+        }
+        return result
